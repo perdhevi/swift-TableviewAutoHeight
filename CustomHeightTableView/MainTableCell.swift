2c7722e243bbf56a304aa1285046084c8ba118ac
Raditya Perdhevi <raditya.perdhevi@gmail.com>
2017-11-21 06:58:39 +0700

    initial commit

---
+++ CustomHeightTableView/MainTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableCell.swift
+//  CustomHeightTableView
+//
+//  Created by raditya perdhevi on 20/11/17.
+//  Copyright © 2017 Perdhevi. All rights reserved.
+//
+
+import UIKit
+
+class MainTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
